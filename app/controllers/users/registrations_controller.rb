ab0f2a63ff39185f05e32fddcbd20101c8443d05
daniel leal <danielleal94@gmail.com>
2016-01-02 15:24:55 -0300

    adicionado a coluna de imagem de perfil do usuario utilizando o refile para upload de arquivos; adicionado o simple_form para manipulacao dos formularios; gerado as views do devise e sobrescrito o metodo de sign_up e account_update do devise #4

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,16 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u|
+      u.permit(:profile_image, :email, :password, :password_confirmation)
+    end
+    devise_parameter_sanitizer.for(:account_update) do |u|
+      u.permit(:profile_image, :email, :password, :password_confirmation, :current_password)
+    end
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Rotas geradas pelo Devise, com adição do omniauth
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: "users/registrations" }
 
   root 'home#index'
 
