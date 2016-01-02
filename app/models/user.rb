9ca451bd5c1600dd99ae42f0f79dfa5ec4ce5207
Douglas Costa <dodop3000@gmail.com>
2016-01-02 22:23:22 +0000

    fixes #4

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.profile_image = open(URI.parse(auth.info.image).to_s)
       user.password = Devise.friendly_token[0,20]
     end
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -238,7 +238,8 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], scope: 'email'
+  config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], 
+    scope: 'email', image_size: { witdh: 500, height: 500 }, secure_image_url: true
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
